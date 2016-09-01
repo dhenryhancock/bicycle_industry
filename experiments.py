3d1afc034192ce5dbbb01e5911584ddfa46ca986
Henry Hancock <dhenryhancock@gmail.com>
2016-09-01 19:25:36 +0000

    Save 5

--- experiments.py
+++ experiments.py
@@ -4,7 +4,12 @@ class bicycle(object):
         self.production_cost=production_cost
         self.weight=weight
 
-bicycles=[bicycle("schwinn", 100, 20), bicycle("speedster", 120, 15)]
+bicycles=[bicycle("schwinn", 100, 20), 
+        bicycle("speedster", 120, 15), 
+        bicycle("trek", 150, 12), 
+        bicycle("giant", 500, 8), 
+        bicycle("fuji", 750, 13), 
+        bicycle("kona", 300, 22),]
 
 class bike_shop(object):
     def __init__(self, store_name, markup):
@@ -14,9 +19,10 @@ class bike_shop(object):
         self.inventory={}
         self.inventory_prices={}
         for bike in bicycles:
+            bike_model=bike.model
             bike_price=bike.production_cost * self.markup
-            self.inventory[bike]=50
-            self.inventory_prices[bike]=bike_price
+            self.inventory[bike_model]=50
+            self.inventory_prices[bike_model]=bike_price
     def available_bikes (self):
         for shopper in shoppers:
             print("Here are the bikes that {} can afford" .format(shopper.customer_name))
@@ -25,6 +31,11 @@ class bike_shop(object):
                     print (key)
                 else:
                     pass
+    def sell_bike (self):
+        for people in customer_selections:
+            selection=customer_selections(people)
+            self.inventory[selection]-=1
+            self.profit+=self.inventory_prices[selection]
 
 class customers(object):
     def __init__(self, customer_name, budget, bikes_owned):
@@ -36,6 +47,7 @@ toms_store=bike_shop("toms_store", 1.25)
 
 shoppers=[customers("Sally", 200, []), customers("Jim", 500, []), customers("Jane", 1000, [])]
 
+customer_selections={
 
 toms_store.purchase_inventory()
 
